6b636abe6e67fa277eeb70402450f79e667036cd
majik <me@yamajik.com>
2019-06-27 15:56:20 +0800

    update run.py

--- run.py
+++ run.py
@@ -2,28 +2,19 @@
 from __future__ import absolute_import, print_function
 
 import argparse
-import importlib
 import sys
 
-
-def load(name):
-    try:
-        module_name, component_name = name.rsplit(".", 1)
-        module = importlib.import_module(module_name)
-        return getattr(module, component_name)
-    except Exception as e:
-        print("Load component error!", file=sys.stderr, flush=True)
-        raise e
+import suanpan
 
 
-def run(name, args):
-    return load(name)(*args)
-
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("component")
     args, rest = parser.parse_known_args()
 
     sys.argv = sys.argv[:1]
-    run(args.component, rest)
+    return suanpan.run(args.component, rest)
+
+
+if __name__ == "__main__":
+    main()

a837587512e18c10bdfd1a8f82da357063fe189a
majik <me@yamajik.com>
2020-02-26 10:46:27 +0800

    update

--- tools/init.sh
+++ tools/init.sh
@@ -3,7 +3,7 @@
 set -e
 
 PROJECT_NAME=${SUANPAN_PROJECT_NAME:-"my-comopnents"}
-GIT_REMOTE_ORIGIN=${SUANPAN_GIT_REMOTE_ORIGIN}
+GIT_REMOTE_ORIGIN=${SUANPAN_PROJECT_GIT_REMOTE_ORIGIN}
 
 git clone --depth 1 https://github.com/yamajik/AiLab-Components-Demo ${PROJECT_NAME}
 cd ${PROJECT_NAME}
@@ -14,12 +14,9 @@ sed -i "" "s/my-comopnents/${PROJECT_NAME}/g" README.md || sed -i "s/my-comopnen
 
 echo 'version = "0.0.0"' > components/__init__.py
 git init && git add . && git commit -m "init" && git tag "0.0.0"
-if [ -n "${GIT_REMOTE_ORIGIN}" ]; then
-    git remote set-url --push origin ${GIT_REMOTE_ORIGIN}
-fi
-
 git flow init -d
 if [ -n "${GIT_REMOTE_ORIGIN}" ]; then
-    git branch --set-upstream origin/develop && git push --all && git push --tags
+    git remote add origin ${GIT_REMOTE_ORIGIN}
+    git push -u --all && git push --tags
 fi
 echo "${PROJECT_NAME} init finished!"
--- tools/publish.sh
+++ tools/publish.sh
@@ -8,7 +8,7 @@ UPGRADE_VERSION=$2
 set -e
 
 git flow release start ${UPGRADE_VERSION}
-sed -i "" "s/${CURRENT_VERSION}/${UPGRADE_VERSION}/g" README.md || sed -i "s/${CURRENT_VERSION}/${UPGRADE_VERSION}/g" README.md
+sed -i "" "s/${CURRENT_VERSION}/${UPGRADE_VERSION}/g" components/__init__.py || sed -i "s/${CURRENT_VERSION}/${UPGRADE_VERSION}/g" components/__init__.py
 git commit -a -m "Ver: ${UPGRADE_VERSION}"
 git flow release finish -m "${UPGRADE_VERSION}" ${UPGRADE_VERSION}
 git push --all && git push --tags

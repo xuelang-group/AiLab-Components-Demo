e4e3dfb648217419e1e0d88280d047f14519c36d
renweiwen <renweiwen891017@gmail.com>
2021-04-06 18:17:32 +0800

    add power shell build script

---
+++ tools/build.ps1
@@ -0,0 +1,10 @@
+$ARCH = "amd64"
+
+$REGISTRY ="registry.cn-shanghai.aliyuncs.com"
+$NAMESPACE = "shuzhi-${ARCH}"
+$IMAGE_NAME = $args
+$IMAGE_URL = "${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}"
+$PYTHON_VERSION = "3.7"
+
+docker build -t ${IMAGE_URL}:latest . -f .\docker\Dockerfile --build-arg PYTHON_VERSION=3.7 
+docker push ${IMAGE_URL}:latest

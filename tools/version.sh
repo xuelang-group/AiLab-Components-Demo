208ddae9ad4cb8ab98e069abdaeedfa4fe5b4598
renweiwen <renweiwen891017@gmail.com>
2021-04-06 17:25:57 +0800

    fix no tag situation

--- tools/version.sh
+++ tools/version.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git describe --abbrev=0 --tags master || git describe --abbrev=0 --tags origin/master
+tags=$(git tag)
+if [ -z "$tags" ]; then
+    echo "0.0.1"
+else
+    git describe --abbrev=0 --tags master || git describe --abbrev=0 --tags origin/master
+fi
